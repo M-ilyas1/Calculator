5cd2f69be793eb784b936642e7d423564c4af89e
M-ilyas1 <mohammadilyas6800@gmail.com>
2022-10-28 00:04:56 -0700

    calculator

---
+++ app.js
@@ -0,0 +1,21 @@
+
+let buttons =  document.querySelectorAll('button')
+
+let display  = document.querySelector('.display')
+
+buttons.forEach(function(button){
+    button.addEventListener("click", calculate)
+})
+
+function  calculate(event){
+    const buttonclickValue = event.target.value
+    if(buttonclickValue === "="){
+        if(display.value !==""){
+            display.value = eval(display.value)
+        }
+    }else if(buttonclickValue === "C"){
+        display.value = ""
+    }else{
+        display.value += buttonclickValue
+    }
+}
