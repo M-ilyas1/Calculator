da4b1c4fc2003f083cbcfad4f6c681a31a64bd4d
M-ilyas1 <mohammadilyas6800@gmail.com>
2022-11-01 09:53:14 -0700

    add

--- app.js
+++ app.js
@@ -259,6 +259,29 @@
 
 //12. ============================================================================================================
 
+// let display = document.querySelector(".display")
+
+// let buttons = document.querySelectorAll("button")
+
+// buttons .forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !== ""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+//13. ============================================================================================================
+
 let display = document.querySelector(".display")
 
 let buttons = document.querySelectorAll("button")
@@ -279,8 +302,6 @@ function calculate(event){
         display.value += buttonclickValue
     }
 }
-
-//13. ============================================================================================================
 
 //14. ============================================================================================================
 
