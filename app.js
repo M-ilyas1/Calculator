789ee557f0920f42043f6d2c2c4088505e8d2bfb
M-ilyas1 <mohammadilyas6800@gmail.com>
2022-10-31 23:53:28 -0700

    add

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
-
 // 1. ============================================================================================================
 
 
-let buttons =  document.querySelectorAll('button')
+// let buttons =  document.querySelectorAll('button')
 
-let display  = document.querySelector('.display')
+// let display  = document.querySelector('.display')
+
+// buttons.forEach(function(button){
+//     button.addEventListener("click", calculate)
+// })
+
+// function  calculate(event){
+//     const buttonclickValue = event.target.value
+//     if(buttonclickValue === "="){
+//         if(display.value !==""){
+//             display.value = eval(display.value)
+//         }
+//     }else if(buttonclickValue === "C"){
+//         display.value = ""
+//     }else{
+//         display.value += buttonclickValue
+//     }
+// }
+
+
+// 2. ============================================================================================================
+
+let display = document.querySelector(".display")
+
+let buttons = document.querySelectorAll("button")
 
 buttons.forEach(function(button){
     button.addEventListener("click", calculate)
 })
 
-function  calculate(event){
+function calculate(event){
     const buttonclickValue = event.target.value
     if(buttonclickValue === "="){
-        if(display.value !==""){
+        if(display.value !== ""){
             display.value = eval(display.value)
         }
     }else if(buttonclickValue === "C"){
@@ -22,8 +45,3 @@ function  calculate(event){
         display.value += buttonclickValue
     }
 }
-
-
-// 2. ============================================================================================================
-
-
